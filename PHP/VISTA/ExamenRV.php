416725bc7835ed7068911d5df03db664c5877ec6
CabanillasAlvaro <ctorresalvar@crece.uss.edu.pe>
2021-11-17 16:58:10 -0500

    Cambios de Alvaro

---
+++ PHP/VISTA/ExamenRV.php
@@ -0,0 +1,326 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "Extensiones/Cabeza.php"; ?>
+    <link rel="stylesheet" href="/MOCUNM/CSS/Examen.css">
+</head>
+<body>
+    <?php include "Extensiones/menu.php"; ?>
+    
+    <div class="container col-10 card card-primary card-outline elevation-2">
+        <div id='title'>
+            <hr  width="100%" size="5" noshade>  
+            <h2>EXAMEN DE ADMISIÓN UNM 2022</h2> 
+            <h2>PRUEBA TIPO DE RAZONAMIENTO VERBAL</h2>
+            <hr  width="100%" size="5" noshade>  
+        </div>
+
+        <h3>Lee el texto atentamente y marca la alternativa que corresponde.</h3>
+        <h3>INTERPRETACIÓN DE LECTURA.</h3>
+
+        <h3>Se suele considerar que la música medieval comenzó con los primeros intentos de escritura musical, que se dieron en el siglo VII con el uso de los neumas, los cuales mostraban el contorno de la línea melódica, pero sin precisar ni los intervalos ni los ritmos. El fin de la Edad Media musical se sitúa en el momento en que sobreviene un cambio en todas las artes, que en la música se traduce por un mayor sentido tonal y armónico, menor rigidez en las formas y un nuevo tipo de relación entre el texto y la melodía. Estos cambios que darían paso al Renacimiento, se suelen ubicar aproximadamente en 1450.  Durante la Edad Media las artes en general estaban divididas en religiosas y profanas.</h3>
+
+        <form>
+            <div class="form-group">
+                <div class='pregunta' id='pregunta1'> <h3><img src="" alt=""> </h3></div>
+                <div class='container'>
+                    <div class='row' >                       
+                        <div class='col-1' ></div>
+                        <div class="form-check  col">
+                            <input type="radio" class="respuesta1 form-check-input" value="A" id="0101" name="materialExampleRadios">
+                            <label class="respuesta1 form-check-label" for="0101">La música medieval era menos rígida que la renacentista</label>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="radio" class="respuesta1 form-check-input" value="B" id="0102" name="materialExampleRadios" >
+                            <label class="respuesta1 form-check-label" for="0102">Durante la Edad Media las artes podían dedicarse sea Dios o a los instintos humanos.</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="radio" class="respuesta1 form-check-input" value="C" id="0103" name="materialExampleRadios" >
+                            <label class="respuesta1 form-check-label" for="0103">La Edad Media termina en el siglo XIV.</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="radio" class="respuesta1 form-check-input" value="D" id="0104" name="materialExampleRadios" >
+                            <label class="respuesta1 form-check-label" for="0104">Los neumas eran una forma de escritura exacta.</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                   
+                </div>
+            </div>
+            <!----------------->
+
+            <h3>RELACIONES ANALÓGICAS</h3>
+            <h3>2. Marca la alternativa que cumpla con la relación establecida en las palabras base.</h3>
+            <div class="form-group">
+                <div class='pregunta' id='pregunta2'> <h3><label>ARDIENTE: TIBIO:</label></h3></div>
+                <div class='container'>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="respuesta2 form-check-input" value="A" id="0201" value="1" name="respuesta2">
+                            <label class="form-check-label" for="0201">otoño - invierno</label>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="respuesta2 form-check-input" value="B" id="0202" value="2" name="respuesta2" >
+                            <label class="form-check-label " for="0202">catedral - iglesia</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="form-check-input respuesta2" value="C" id="0203" value="3" name="respuesta2" >
+                            <label class="form-check-label" for="0203">empalagoso - dulce</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="form-check-input respuesta2" value="D" id="0204" value="4" name="respuesta2" >
+                            <label class="form-check-label" for="0204">choza - palacio</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="form-check-input respuesta2" value="E" id="0205" value="4" name="respuesta2" >
+                            <label class="form-check-label" for="0205">vasto – espacioso</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                </div>
+            </div>
+            <!---------------->
+
+            <h3>TÉRMINOS INCLUIDOS</h3>
+            <div class="form-group">
+                <div class='pregunta'  id='pregunta3'> <h3><label>3. ¿Qué opción incluye conceptualmente al resto?</label></h3></div>
+                <div class='container'>
+                    <div class='row'>
+                        <div class='col'>
+                            <div class='row'>                       
+                                <div class='col-1' ></div>
+                                <div class="form-check col">
+                                    <input type="radio" class="respuesta3 form-check-input" value="A" id="0301" name="materialExamplecheckboxs">
+                                    <label class="respuesta3 form-check-label" for="0301">músico</label>
+                                </div>
+                            </div>
+                            <div class='row'>
+                                <div class='col-1' ></div>
+                                <div class="form-check col">
+                                    <input type="radio" class="respuesta3 form-check-input" value="B" id="0302" name="materialExamplecheckboxs" >
+                                    <label class="respuesta3 form-check-label" for="0302">público</label>
+                                </div> 
+                            </div>
+                            <div class='row'>
+                                <div class='col-1' ></div>
+                                <div class="form-check col">
+                                    <input type="radio" class="respuesta3 form-check-input" value="C" id="0303" name="materialExamplecheckboxs" >
+                                    <label class="respuesta3 form-check-label" for="0303">concierto</label>
+                                </div> 
+                            </div>
+                            <div class='row'>
+                                <div class='col-1' ></div>
+                                <div class="form-check col">
+                                    <input type="radio" class="respuesta3 form-check-input" value="D" id="0304" name="materialExamplecheckboxs" >
+                                    <label class="respuesta3 form-check-label" for="0304">aplausos</label>
+                                </div> 
+                            </div>
+                            <div class='row'>
+                                <div class='col-1' ></div>
+                                <div class="form-check col">
+                                    <input type="radio" class="respuesta3 form-check-input" value="E" id="0305" name="materialExamplecheckboxs" >
+                                    <label class="respuesta3 form-check-label" for="0305">sonata</label>
+                                </div> 
+                            </div>
+
+                        </div>
+                        <!--<div class='centrar-content col'>
+                            <audio controls>
+                                <source src="/MOCUNM/AUDIO/audio_prueba.mp3" type="audio/ogg">
+                                <source src="/MOCUNM/AUDIO/audio_prueba.mp3" type="audio/mpeg">
+                                <source src="/MOCUNM/AUDIO/audio_prueba.mp3" type="audio/wav">
+                                
+                                    <object type="application/x-shockwave-flash" data="player.swf?miArchivo=mi-archivo-de-audio.mp3">
+                                        <param name="movie" value="player.swf?miArchivo=mi-archivo-de-audio.mp3">
+                                        <a href="mi-archivo-de-audio.mp3">Descárgate aquí mi archivo de audio</a>
+                                </object>
+                            </audio>
+                        </div>-->
+                    </div>
+                </div>
+            </div>
+
+            <h3>ANTÓNIMOS EN CONTEXTO</h3>
+            <h3>4. Marca la alternativa correcta que corresponda al antónimo de la palabra subrayada.</h3>
+            <div class="form-group">
+                <div class='pregunta' id='pregunta4'> <h3><label>Recuerda que no debes ser indolente al practicar</label></h3></div>
+                <div class='container'>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="respuesta4 form-check-input" value="A" id="0401" value="1" name="respuesta4">
+                            <label class="form-check-label" for="0401">dejado</label>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="respuesta4 form-check-input" value="B" id="0402" value="2" name="respuesta4" >
+                            <label class="form-check-label " for="0402">descuidado</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="form-check-input respuesta4" value="C" id="0403" value="3" name="respuesta4" >
+                            <label class="form-check-label" for="0403">cachazudo</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="form-check-input respuesta4" value="D" id="0404" value="4" name="respuesta4" >
+                            <label class="form-check-label" for="0404">flojo</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <input type="checkbox" class="form-check-input respuesta4" value="E" id="0405" value="4" name="respuesta4" >
+                            <label class="form-check-label" for="0405">activo</label>
+                        </div> 
+                        <div class='col-1' ></div>
+                    </div>
+                </div>
+            </div>
+
+            <!------------------->
+            <!--<div class="form-group">
+                <div class='pregunta' id='pregunta4'> <h3><label>pregunta 4: Respuesta en texto</label></h3></div>
+                <div class='container'>
+                    <div class='centrar-content row'>
+                        <div class='col-1'></div>
+                        <div class='col-8' >
+                            <textarea class="respuesta4 form-control rounded-0" name="" id="" rows="5"></textarea>
+                        </div>
+                        <div class='col-1'></div>
+                    </div>
+                </div>
+            </div>-->
+            <!----------------->
+            <!--<div class="form-group">
+                <div class='pregunta' id='pregunta5'> <h3><label>pregunta 5: Verdadero y Falso</label></h3></div>
+                <div class='container'>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="col">
+                            <div class='row'>
+                                <label class="titlecenter col" >Preguntas</label>
+                                <label class="titlecenter col" >Verdadero</label>
+                                <label class="titlecenter col" >Falso</label>
+                            </div>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <div class=' row'>
+                                <label class="form-check-label col">Pregunta 1</label>
+                                <input type="radio" class="col respuesta5" name="0501">
+                                <input type="radio" class="col respuesta5" name="0501">
+                            </div>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <div class=' row'>
+                                <label class="form-check-label col" >Pregunta 2</label>
+                                <input type="radio" class="col respuesta5" name="0502">
+                                <input type="radio" class="col respuesta5" name="0502">
+                            </div>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <div class=' row'>
+                                <label class="form-check-label col" >Pregunta 3</label>
+                                <input type="radio" class="col respuesta5" name="0503">
+                                <input type="radio" class="col respuesta5" name="0503">
+                            </div>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <div class=' row'>
+                                <label class="form-check-label col" >Pregunta 4</label>
+                                <input type="radio" class="col respuesta5" name="0504">
+                                <input type="radio" class="col respuesta5" name="0504">
+                            </div>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <div class=' row'>
+                                <label class="form-check-label col" >Pregunta 5</label>
+                                <input type="radio" class="col respuesta5" id="0505" name="0505">
+                                <input type="radio" class="col respuesta5" id="0505" name="0505">
+                            </div>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                    <div class='row'>                       
+                        <div class='col-1' ></div>
+                        <div class="form-check col">
+                            <div class=' row'>
+                                <label class="form-check-label col" >Pregunta 6</label>
+                                <input type="radio" class="col respuesta5" id="0506" name="0506">
+                                <input type="radio" class="col respuesta5" id="0506" name="0506">
+                            </div>
+                        </div>
+                        <div class='col-1' ></div>
+                    </div>
+                </div>
+            </div>-->
+            <!------------------------->
+            
+            <div class='centrar-content'>
+                <a type="submit" class="btn btn-success" href="https://academica360-beta.unm.edu.pe/Admisionin/inscripcion/ficha">Grabar Respuestas</a>
+            </div>
+        </form>
+    </div>
+    <?php include "Extensiones/Pie.php"; ?>
+    <script src="/MOCUNM/JS/Examen.js"></script>
+</body>
+</html>
